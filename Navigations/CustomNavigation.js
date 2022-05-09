6d1908e7b90cfab49c31f246cee2906dcc76dcb9
dharmendra841434 <dhk7283013841@gmail.com>
2022-05-09 17:43:04 +0530

    changes done

--- Navigations/CustomNavigation.js
+++ Navigations/CustomNavigation.js
@@ -34,17 +34,6 @@ import CreatePost from '../Screens/RoomScreens/CreatePost';
 const Stack = createNativeStackNavigator();
 
 export const HomeStack = () => {
-  const config = {
-    animation: 'slide',
-    config: {
-      stiffness: 1000,
-      damping: 500,
-      mass: 3,
-      overshootClamping: true,
-      restDisplacementThreshold: 0.01,
-      restSpeedThreshold: 0.01,
-    },
-  };
   return (
     <Stack.Navigator>
       <Stack.Screen
--- Screens/HomeScreens/TopTabScreens/ArticleScreen2.js
+++ Screens/HomeScreens/TopTabScreens/ArticleScreen2.js
@@ -2,8 +2,10 @@ import {View, Text, StyleSheet, Dimensions, ScrollView} from 'react-native';
 import React, {useContext, useState} from 'react';
 import HeaderCard from '../../../Componants/HomeScreenComponants/ArticalScreenComponants/HeaderCard';
 import ArticalList2 from '../../../Componants/HomeScreenComponants/ArticalScreenComponants/ArticalList2';
-import { SavedArticleContext } from '../../../App';
-
+import {SavedArticleContext} from '../../../App';
+import AppColors from '../../../Constaint/AppColors';
+import Backbtn from '../../../Componants/Backbtn';
+import {useNavigation} from '@react-navigation/native';
 
 const Height = Dimensions.get('window').height;
 const Width = Dimensions.get('window').width;
@@ -11,10 +13,19 @@ const Width = Dimensions.get('window').width;
 console.log(Height);
 
 const ArticleScreen2 = props => {
-  const {savedarticlestate,savedarticledispatch} = useContext(SavedArticleContext);
+  const {savedarticlestate, savedarticledispatch} =
+    useContext(SavedArticleContext);
+
+  const navigation = useNavigation();
 
   return (
     <View style={styles.screen}>
+      <Backbtn
+        IconSize={30}
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
       <View style={styles.ListContainer}>
         <ArticalList2 data={savedarticlestate} />
       </View>
@@ -23,9 +34,9 @@ const ArticleScreen2 = props => {
 };
 const styles = StyleSheet.create({
   screen: {
-    display:'flex',
-    flex:1,
-    backgroundColor:'black',
+    display: 'flex',
+    flex: 1,
+    backgroundColor: AppColors.primarycolor,
   },
   ListContainer: {
     paddingHorizontal: 20,
